b6832467b527267e2ae79cec25628295a51c10ed
sl4gr00m <sl4gr00m@gmail.com>
2018-08-10 19:08:37 -0300

    Make ConfigurationValueFormatException an Exception
    Whoops

--- wimm.Confoundry.UnitTests/Exceptions/ConfigurationValueFormatExceptionTest.cs
+++ wimm.Confoundry.UnitTests/Exceptions/ConfigurationValueFormatExceptionTest.cs
@@ -8,9 +8,10 @@ namespace wimm.Confoundry.UnitTests.Exceptions
     {
         private const string _name = "Configuration Name";
         private const string _value = "Configuration Value";
+        private static readonly Exception _exception = new Exception();
 
         [Fact]
-        public void Construct_NullName_Throws()
+        public void ConstructNameValue_NullName_Throws()
         {
             var ex = Assert.Throws<ArgumentNullException>(() =>
             {
@@ -24,7 +25,7 @@ namespace wimm.Confoundry.UnitTests.Exceptions
         [InlineData("\t")]
         [InlineData("\n")]
         [InlineData("\r\n")]
-        public void Construct_WhiteSpaceName_Throws(string whiteSpace)
+        public void ConstructNameValue_WhiteSpaceName_Throws(string whiteSpace)
         {
             var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
             {
@@ -33,7 +34,7 @@ namespace wimm.Confoundry.UnitTests.Exceptions
         }
 
         [Fact]
-        public void Construct_NullValue_Throws()
+        public void ConstructNameValue_NullValue_Throws()
         {
             var ex = Assert.Throws<ArgumentNullException>(() =>
             {
@@ -42,13 +43,24 @@ namespace wimm.Confoundry.UnitTests.Exceptions
         }
 
         [Fact]
-        public void Construct_ValidArgs_Constructs()
+        public void ConstructNameValue_ValidArgs_Constructs()
         {
             var _ = new ConfigurationValueFormatException(_name, _value);
         }
 
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        [InlineData("\n")]
+        [InlineData("\r\n")]
+        public void ConstructNameValue_WhiteSpaceValue_Constructs(string whiteSpace)
+        {
+            var _ = new ConfigurationValueFormatException(_name, whiteSpace);
+        }
+
         [Fact]
-        public void Name_Constructed_HasNameArgumentValue()
+        public void Name_ConstructNameValue_HasNameArgumentValue()
         {
             var underTest = new ConfigurationValueFormatException(_name, _value);
 
@@ -58,7 +70,7 @@ namespace wimm.Confoundry.UnitTests.Exceptions
         }
 
         [Fact]
-        public void Value_Constructed_HasValueArgumentValue()
+        public void Value_ConstructNameValue_HasValueArgumentValue()
         {
             var underTest = new ConfigurationValueFormatException(_name, _value);
 
@@ -67,15 +79,96 @@ namespace wimm.Confoundry.UnitTests.Exceptions
             Assert.Equal(_value, actual);
         }
 
+
+
+
+
+        [Fact]
+        public void ConstructNameValueException_NullName_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = new ConfigurationValueFormatException(null, _value, _exception);
+            });
+        }
+
         [Theory]
         [InlineData("")]
         [InlineData(" ")]
         [InlineData("\t")]
         [InlineData("\n")]
         [InlineData("\r\n")]
-        public void Construct_WhiteSpaceValue_Constructs(string whiteSpace)
+        public void ConstructNameValueException_WhiteSpaceName_Throws(string whiteSpace)
         {
-            var _ = new ConfigurationValueFormatException(_name, whiteSpace);
+            var ex = Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                var _ = new ConfigurationValueFormatException(whiteSpace, _value, _exception);
+            });
+        }
+
+        [Fact]
+        public void ConstructNameValueException_NullValue_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = new ConfigurationValueFormatException(_name, null, _exception);
+            });
+        }
+
+        [Fact]
+        public void ConstructNameValueException_ValidArgs_Constructs()
+        {
+            var _ = new ConfigurationValueFormatException(_name, _value, _exception);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("\t")]
+        [InlineData("\n")]
+        [InlineData("\r\n")]
+        public void ConstructNameValueException_WhiteSpaceValue_Constructs(string whiteSpace)
+        {
+            var _ = new ConfigurationValueFormatException(_name, whiteSpace, _exception);
+        }
+
+        [Fact]
+        public void ConstructNameValueException_NullException_Throws()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                var _ = new ConfigurationValueFormatException(_name, _value, null);
+            });
+        }
+
+        [Fact]
+        public void Name_ConstructNameValueException_HasNameArgumentValue()
+        {
+            var underTest = new ConfigurationValueFormatException(_name, _value, _exception);
+
+            var actual = underTest.Name;
+
+            Assert.Equal(_name, actual);
+        }
+
+        [Fact]
+        public void Value_ConstructNameValueException_HasValueArgumentValue()
+        {
+            var underTest = new ConfigurationValueFormatException(_name, _value, _exception);
+
+            var actual = underTest.Value;
+
+            Assert.Equal(_value, actual);
+        }
+
+        [Fact]
+        public void InnerException_ConstructNameValueException_HasInnerExceptionArgumentValue()
+        {
+            var underTest = new ConfigurationValueFormatException(_name, _value, _exception);
+
+            var actual = underTest.InnerException;
+
+            Assert.Same(_exception, actual);
         }
     }
 }
--- wimm.Confoundry/Exceptions/ConfigurationValueFormatException.cs
+++ wimm.Confoundry/Exceptions/ConfigurationValueFormatException.cs
@@ -6,7 +6,7 @@ namespace wimm.Confoundry.Exceptions
     /// Thrown when the value given for a <see cref="Configuration{T}"/> cannot be used to instantiate an instance of
     /// the configuration's value type.
     /// </summary>
-    public class ConfigurationValueFormatException
+    public class ConfigurationValueFormatException : Exception
     {
         /// <summary>
         /// The name of the <see cref="Configuration{T}"/>.
@@ -30,6 +30,31 @@ namespace wimm.Confoundry.Exceptions
         /// <paramref name="name"/> is white space.
         /// </exception>
         public ConfigurationValueFormatException(string name, string value)
+            : base($"Invalid value format for Configuration<T> '{name}'")
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentOutOfRangeException(nameof(name),
+                    "Must contain at least one non-whitespace character");
+
+            Value = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="ConfigurationValueFormatException"/>.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="Configuration{T}"/> that was being constructed.</param>
+        /// <param name="value">The value that was given.</param>
+        /// <param name="innerException">The exception that occured while attempting to initialize the value.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="name"/>, <paramref name="value"/>, or <paramref name="innerException"/> is <c>null</c>.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="name"/> is white space.
+        /// </exception>
+        public ConfigurationValueFormatException(string name, string value, Exception innerException)
+            : base($"Invalid value format for Configuration<T> '{name}'", 
+                  innerException ?? throw new ArgumentNullException(nameof(innerException)))
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
             if (string.IsNullOrWhiteSpace(name))
